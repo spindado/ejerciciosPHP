4041ab21867d029214b4cc317fee287548c5f72b
spindado <sergio.pindadog@gmail.com>
2019-11-14 09:51:35 +0100

    ....

---
+++ ejercicioCookies/php/ver_formulario.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="../css/style.css" />
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <form action="" method="">
+            <fieldset>
+                <label for="a">Languaje</label>
+                <select name="lang" id="">
+                    <option value="">es</option>
+                    <option value="">ing</option>
+                </select>
+
+            </fieldset>
+            <fieldset>
+                <label for="b">Background-color</label>
+                <select name="back-color" id="">
+                    <option value="">white</option>
+                    <option value="">green</option>
+                    <option value="">black</option>
+                    <option value="">red</option>
+                </select>
+
+            </fieldset>
+            <fieldset>
+                <label for="c">Font-color</label>
+                <select name="font-color" id="">
+                    <option value="">black</option>
+                    <option value="">white</option>
+                    <option value="">blue</option>
+                </select>
+
+            </fieldset>
+        </form>
+
+    </div>
+
+
+    <?php
+    if (isset($_COOKIE)) { }
+
+    ?>
+
+</body>
+
+</html>
