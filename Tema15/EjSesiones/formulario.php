4ad8a3992b59681e87c956ab209b2209309c4ff1
spindado <sergio.pindadog@gmail.com>
2019-11-30 13:55:13 +0100

    .

--- Tema15/EjSesiones/formulario.php
+++ Tema15/EjSesiones/formulario.php
@@ -1,44 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css" />
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+      <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-    <div id="login-form">
-<form method="post">
-<table >
-<?php
-if(isset($error))
-{
- ?>
-    <tr>
-    <td id="error"><?php echo $error; ?></td>
-    </tr>
-    <?php
-}
-?>
-<tr>
-<td><input type="text" name="uname" placeholder="User Name" value="<?php if(isset($uname)){echo $uname;} ?>"  <?php if(isset($code) && $code == 1){ echo "autofocus"; }  ?> /></td>
-</tr>
-<tr>
-<td><input type="text" name="email" placeholder="Your Email"  value="<?php if(isset($email)){echo $email;} ?>" <?php if(isset($code) && $code == 2){ echo "autofocus"; }  ?> /></td>
-</tr>
-<tr>
-<td><input type="password" name="pass" placeholder="Your Password" <?php if(isset($code) && $code == 4){ echo "autofocus"; }  ?> /></td>
-</tr>
-<tr>
-<td><button type="submit" name="btn-signup">Sign Me Up</button></td>
-</tr>
-</table>
-</form>
-</div>
-
-
-    
+      
+      <div id="login-form">
+            <form action="" method="post">
+                  <table>
+                        <tr>
+                              <td>
+                                    <input type="text" name="uname" placeholder="User Name" value="<?php if(isset($uname)){echo $uname;} ?>"  <?php if(isset($code) && $code == 1){ echo "autofocus"; }  ?> />
+                              </td>
+                        </tr>
+                        <tr>
+                              <td>
+                                    <input type="password" name="pass" placeholder="Your Password" <?php if(isset($code) && $code == 4){ echo "autofocus"; }  ?> />
+                              </td>
+                        </tr>
+                        <tr>
+                              <td>
+                                    <button type="submit" name="btn-signup">Sign Me Up</button>
+                              </td>
+                        </tr>
+                        
+                        
+                  </table>
+            </form>
+      </div>
+      
 </body>
 </html>
