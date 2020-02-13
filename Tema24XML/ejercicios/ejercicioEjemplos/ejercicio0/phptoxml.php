555cc2f5a39b5379989678b73658111f34024d01
spindado <sergio.pindadog@gmail.com>
2020-02-13 11:10:15 +0100

    adds XML examples

--- Tema24XML/ejercicios/ejercicioEjemplos/ejercicio0/phptoxml.php
+++ Tema24XML/ejercicios/ejercicioEjemplos/ejercicio0/phptoxml.php
@@ -1,6 +1,7 @@
 <?php 
+include("ejercicio0.php");
 //function defination to convert array to xml
-function array_to_xml($array, &$xml_user_info) {
+function array_to_xml($array, $xml_user_info) {
     foreach($array as $key => $value) {
         if(is_array($value)) {
             if(!is_numeric($key)){
@@ -17,7 +18,7 @@ function array_to_xml($array, &$xml_user_info) {
 }
 
 //creating object of SimpleXMLElement
-$xml_user_info = new SimpleXMLElement("<?xml version="1.0"?><user_info></user_info>");
+$xml_user_info = new SimpleXMLElement("<?xml version='1.0'?> <user_info></user_info>");
 
 //function call to convert array to xml
 array_to_xml($users_array,$xml_user_info);
